59caf1b70da2961e4f232a89e2102bd25e7eaac5
Admin <Admin@DESKTOP-EQ55Q8H>
2026-08-12 11:59:31 +0530

    httpservlet method and also bootstrap intro program using database

---
+++ ServetAPI/src/main/java/HttpSessionServlet.java
@@ -0,0 +1,39 @@
+
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/HttpSessionServlet")
+public class HttpSessionServlet extends HttpServlet {
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		out.print("<h1>Welcome to Servlet</h1>");
+		
+		String username = request.getParameter("UserName");
+		String password = request.getParameter("Password");
+		
+		out.println("Name is : "+username+"<br>");
+		out.println("City is : "+password+"<br>");
+		
+		HttpSession h=request.getSession();
+		h.setAttribute("password is:", password);
+		h.setAttribute("username is:", username);
+		
+		out.print("<a href='HttpSessionServlet2'> click me </a>");
+	}
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
---
+++ ServetAPI/src/main/java/HttpSessionServlet2.java
@@ -0,0 +1,37 @@
+
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/HttpSessionServlet2")
+public class HttpSessionServlet2 extends HttpServlet {
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html");
+		PrintWriter out = response.getWriter();
+		out.print("<h1>Welcome to Servlet</h1>");
+		
+		String username = request.getParameter("UserName");
+		String password = request.getParameter("Password");
+		
+		out.println("Name is : "+username+"<br>");
+		out.println("City is : "+password+"<br>");
+		
+		HttpSession h1=request.getSession(false);
+		h1.getAttribute("password is:");
+		h1.getAttribute("username is:");
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
